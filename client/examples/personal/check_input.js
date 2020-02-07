766142d3da716adde192f2a8000ec9b93a010718
heejin342 <53699301+heejin342@users.noreply.github.com>
2020-02-07 20:08:12 +0900

    test2

---
+++ client/examples/personal/check_input.js
@@ -0,0 +1,34 @@
+FlowRouter.template('/check_input', 'check_input');
+
+
+Template.ex_login_navbar.events({
+    'click #btn-start': function () {
+        //     var userInfo = Meteor.user();
+        //     var name = $('#inp-name').val();
+        //     var address = $('#inp-address').val();
+        //     var mobile = $('#inp-mobile').val();
+        //
+        //     Meteor.users.update({_id: userInfo._id}, {
+        //         $set: { // 사용자 객체의 profile 프로퍼티는 사용자 기타 정보를 저장 하는 공통 된 위치 입니다.
+        //             'username': name,
+        //             'profile.address': address,
+        //             'profile.mobile': mobile
+        //         }
+        //     });
+        //
+        //     alert('사용자 프로파일을 수정 하였습니다.');
+        // }
+
+        // var imgArray = new Array();
+        // imgArray[0] = "examples/woman.png";
+        // imgArray[1] = "examples/woman (1).png";
+        // imgArray[2] = "examples/woman (2).png";
+        // imgArray[4] = "examples/woman (3).png";
+        // imgArray[4] = "examples/woman (4).png";
+        // imgArray[5] = "examples/woman (5).png";
+        // imgArray[6] = "examples/woman (6).png";
+        // imgArray[7] = "examples/woman (7).png";
+
+    }
+});
+
