f3bdd89834a29027f8c4fbb022a840e6c1241461
nahye <nahelove03@gmail.com>
2020-02-26 06:11:21 +0900

    .

--- client/examples/personal/reco_lip.js
+++ client/examples/personal/reco_lip.js
@@ -1,12 +1,6 @@
 FlowRouter.template('/reco_lip', 'reco_lip');
 
-
-Template.reco_lip_info.onRendered(function () {
-
-});
-
 Template.reco_lip_info.helpers({
-
     toneInfo: function() {
         var userTone = Meteor.user().profile.p_ton;
 
@@ -39,20 +33,30 @@ Template.reco_lip_info.events({
     'click #btn_addcartOne' : function () {
         // var tonename = Session.get('tonename');
         var userInfo = Meteor.user();
-        var cos_cnt = $('#inputGroupSelect04').val();
-        var cos_name = $('.card-text').text().split("\n")[0];
-        var cos_image = $('.card-image-top').val();
+        var cos_cnt = $('#inputGroupSelect04').val()
+        var cos_name = $('#cm_name').val()
+
+        // var target = document.getElementById("inputGroupSelect04");
+        // var cos_cnt = target.options[target.selectedIndex].value
+        // var cos_name = $('#cm_name').val()
+        // var cos_name = $('.card-text').text().split("\n")[0];
+        // var cos_image = $('.card-image-top').val();
         // var cos_name = DM_COSM.findOne({cm_class:"lip" , cm_tone:tonename})[0].cm_name;
 
+        // Meteor.users.update({_id: userInfo._id}, {
+        //     $set: { // 사용자 객체의 profile 프로퍼티는 사용자 기타 정보를 저장 하는 공통 된 위치 입니다.
+        //         'cart.cos.name': cos_name,
+        //         'cart.cos.cnt': cos_cnt,
+        //     }
+        // });
 
+        DB_CART.insert({
+            cart_user : userInfo._id,
+            cos_cnt : cos_cnt,
+            cos_name : cos_name
+        })
 
-        Meteor.users.update({_id: userInfo._id}, {
-            $set: { // 사용자 객체의 profile 프로퍼티는 사용자 기타 정보를 저장 하는 공통 된 위치 입니다.
-                'cart.cos.name': cos_name,
-                'cart.cos.cnt': cos_cnt,
-            }
-        });
         alert("장바구니에 추가되었습니다");
-    },
+    }
 });
 
